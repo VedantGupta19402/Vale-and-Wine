56f79260ec9c292a126e6dbe8c4000df8b62ac08
VedantGupta19402 <vedantguptav@gmail.com>
2026-02-13 20:49:33 +0530

    second commit

--- killer dripster/src/components/Navbar.jsx
+++ killer dripster/src/components/Navbar.jsx
@@ -1,11 +1,32 @@
+
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='h-15 bg-amber-500'>
-      hello
+    <div>
+      
+<div className="main h-28 flex bg-[#DCDAD7] justify-between items-center">
+    <div className='h-13 w-20'>
+        <img src='.' alt='' />
+        {/* will do later */}
+    </div>
+
+
+    <div className=' h-15 w-130 flex items-center justify-between font-medium text-none'>
+        <a href="#" className=''>Man</a>
+        <a href="#">Women</a>
+        <a href="#">Children</a>
+        <a href="#">Athletics</a>
+        <a href="#">Explore</a>
+    </div>
+    <div>
+{/* icons do later */}
+    </div>
+</div>
+
     </div>
   )
 }
-
+// 629FAD
 export default Navbar
+
